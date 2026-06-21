9832c5eb8f4588c76f8e4f80ff1e8edc3aaadfd9
areeba-cyber <arebareeba409@gmail.com>
2026-06-21 07:00:38 +0500

    Guess number

--- Guess The NUmber/numberguess.py
+++ Guess The NUmber/numberguess.py
@@ -4,9 +4,9 @@ HARD_LEVEL_ATTEMPTS = 5
 
 def set_difficulty(level_chosen):
     if level_chosen == 'easy':
-     attempts = 10
+     return EASY_LEVEL_ATTEMPTS
     else:
-     attempts = 5
+     return HARD_LEVEL_ATTEMPTS
 
 
 print("Let me think of a number between 1 to 50")
@@ -14,4 +14,7 @@ answer = random.randint(1, 50)
 print(answer)
 
 Level = input("Choose the level of difficulty... Type 'easy' or 'hard'")
-set_difficulty (Level)
+attempts = set_difficulty (Level)
+
+print(f"You have {attempts} reamaining attempts to guess a number")
+guessed_number = input(int("Guess Number:"))
