0559e9b5ed26d3edbdacbf9949f7272f4f420e9d
areeba-cyber <arebareeba409@gmail.com>
2026-06-21 07:08:23 +0500

    Attempts

--- Guess The NUmber/numberguess.py
+++ Guess The NUmber/numberguess.py
@@ -8,6 +8,15 @@ def set_difficulty(level_chosen):
     else:
      return HARD_LEVEL_ATTEMPTS
 
+def check_answer(guessed_number, answer, attempts):
+  if guessed_number < answer:
+    print("Your guess is LOW")
+    return attempts-1
+  elif guessed_number > answer:
+    print("Your guess is HIGH")
+    return attempts-1
+
+
 
 print("Let me think of a number between 1 to 50")
 answer = random.randint(1, 50)
@@ -18,3 +27,4 @@ attempts = set_difficulty (Level)
 
 print(f"You have {attempts} reamaining attempts to guess a number")
 guessed_number = input(int("Guess Number:"))
+check_answer(guessed_number, answer, attempts)
