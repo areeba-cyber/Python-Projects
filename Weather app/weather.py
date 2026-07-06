a28d09ed5d0cf1d3c5a0cdbb9855aaf378c690ca
areeba-cyber <arebareeba409@gmail.com>
2026-07-06 15:12:42 +0500

    import request

---
+++ Weather app/weather.py
@@ -0,0 +1,7 @@
+import requests
+
+# city_name = input("Enter city name: ")
+# API_key = '9b3f643fb121093e79165f75b00edd9e'
+
+# url = "https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units={metric}"
+
