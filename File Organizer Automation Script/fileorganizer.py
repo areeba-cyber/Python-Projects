8316cb92626478febce71b26a52def1b539f118c
areeba-cyber <arebareeba409@gmail.com>
2026-07-09 15:41:30 +0500

    file types

--- File Organizer Automation Script/fileorganizer.py
+++ File Organizer Automation Script/fileorganizer.py
@@ -3,3 +3,13 @@ import shutil
 
 # Folder to organize
 folder_path = input("Enter folder path: ")
+# File type categories
+file_types = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp"],
+    "Documents": [".pdf", ".docx", ".doc", ".txt", ".pptx", ".xlsx"],
+    "Videos": [".mp4", ".mkv", ".avi", ".mov"],
+    "Music": [".mp3", ".wav", ".aac"],
+    "Archives": [".zip", ".rar", ".7z"],
+    "Python Files": [".py"],
+    "Others": []
+}
