d5ae88d1b09f98e8b67c651b83684a0fad3d0568
areeba-cyber <arebareeba409@gmail.com>
2026-07-14 16:06:41 +0500

    print

--- Web Scraper/main.py
+++ Web Scraper/main.py
@@ -8,3 +8,5 @@ with open('home.html', 'r') as html_file:
    for course in course_cards:
       course_name = course.h5.text
       course_price = course.a.text.split()[-1]
+
+      print(f"{course_name} costs {course_price}")
