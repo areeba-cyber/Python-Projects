3126e1ad1856161ebd639649291592f45f572084
areeba-cyber <arebareeba409@gmail.com>
2026-07-18 21:24:46 +0500

    company name

--- Web_Scraper/Web_scraping_from_website/main.py
+++ Web_Scraper/Web_scraping_from_website/main.py
@@ -3,4 +3,5 @@ import requests
 
 html_text = requests.get('https://www.timesjob.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all('li', class_= 'clearfix job-bx wht-shd-bx')
+job = soup.find('li', class_= 'clearfix job-bx wht-shd-bx')
+company_name = job.find('h3', class_= 'joblist-comp-name').text.replace(' ','')
