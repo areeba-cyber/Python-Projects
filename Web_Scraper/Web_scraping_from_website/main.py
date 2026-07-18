bd43c63875fff00d6b89649bf65f76148483274e
areeba-cyber <arebareeba409@gmail.com>
2026-07-18 21:11:10 +0500

    li

--- Web_Scraper/Web_scraping_from_website/main.py
+++ Web_Scraper/Web_scraping_from_website/main.py
@@ -2,3 +2,5 @@ from bs4 import BeautifulSoup
 import requests
 
 html_text = requests.get('https://www.timesjob.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('li', class_= 'clearfix job-bx wht-shd-bx')
