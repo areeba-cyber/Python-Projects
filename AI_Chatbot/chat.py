0ad81c21daf8eaa9b36def822f6d82a8506fd965
areeba-cyber <arebareeba409@gmail.com>
2026-07-25 19:20:02 +0500

    key

--- AI_Chatbot/chat.py
+++ AI_Chatbot/chat.py
@@ -1,6 +1,6 @@
 import os
 from openai import OpenAI
 
-api_key = os.getenv("OPENAI_API_KEY")
-
-client = OpenAI(api_key = api_key)
+client = OpenAI(
+    api_key="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"
+)
