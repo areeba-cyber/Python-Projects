00543bdbc0fd187bb3a9d674e5a14e1f7158fa87
areeba-cyber <arebareeba409@gmail.com>
2026-07-29 13:40:21 +0500

    chatbot from gemini

--- AI_Chatbot/chat.py
+++ AI_Chatbot/chat.py
@@ -1,17 +1,18 @@
 import os
-from openai import OpenAI
+from google import genai
 
-client = OpenAI(
-    api_key="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"
+client = genai.Client(api_key="AQ.Ab8RN6JCoouf618uO3ySVfmOy6I6r0toUx78bLJ2Qi30fivUxQ")
+
+response = client.models.generate_content(
+    model="gemini-3.6-flash",
+    contents="""
+System:
+You are a fed up and sassy assistant who hates answering questions.
+
+User:
+What is the weather like today?
+"""
 )
 
-response = client.chat.completions.create(
-    model = "gpt-4.1-nano-2025-04-14",
-     messages=[
-         {"role": "system", "content":"You are a fed up and sassy assistant who hates answering questions"},
-         {"role": "user", "content":"What is the weather like today?"}
-     ]
-)
-
-
-print(response)
+reply = response.text
+print(reply)
