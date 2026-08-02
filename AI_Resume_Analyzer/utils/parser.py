1f9d8c80dec3a9963e5f918a3fd8b3528d05cb66
areeba-cyber <arebareeba409@gmail.com>
2026-08-02 19:01:56 +0500

    extract

--- AI_Resume_Analyzer/utils/parser.py
+++ AI_Resume_Analyzer/utils/parser.py
@@ -1,2 +1,46 @@
 import fitz
 from docx import Document
+
+# Create a PDF Extraction Function
+def extract_pdf_text(pdf_file):
+
+    text = ""
+
+    pdf = fitz.open(stream=pdf_file.read(), filetype="pdf")
+
+    for page in pdf:
+        text += page.get_text()
+
+    pdf.close()
+
+    return text
+
+# Create a DOCX Extraction Function
+def extract_docx_text(docx_file):
+
+    document = Document(docx_file)
+
+    text = ""
+
+    for paragraph in document.paragraphs:
+
+        text += paragraph.text + "\n"
+
+    return text
+
+# CREATE ONE COMMON FUNCTION
+def extract_resume_text(uploaded_file):
+
+    if uploaded_file.type == "application/pdf":
+
+        return extract_pdf_text(uploaded_file)
+
+    elif uploaded_file.type in [
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    ]:
+
+        return extract_docx_text(uploaded_file)
+
+    else:
+
+        return None
