70c87427267d2d3b0fc32bc87eae1676f779db4e
areeba-cyber <arebareeba409@gmail.com>
2026-08-02 18:19:28 +0500

    load css n sidebar

--- AI_Resume_Analyzer/app.py
+++ AI_Resume_Analyzer/app.py
@@ -1,10 +1,36 @@
 import streamlit as st
 
+
+def load_css():
+
+    with open("css/style.css") as f:
+
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True,
+        )
+
+load_css()
+
+
+with st.sidebar:
+
+    st.title("📄 Resume Analyzer")
+
+    st.markdown("---")
+
+    st.write("🏠 Home")
+
+    st.write("📤 Upload Resume")
+
+    st.write("📊 Dashboard")
+
+    st.write("📈 Reports")
+
+    st.write("⚙ Settings")
+
 st.set_page_config(
     page_title="AI Resume Analyzer",
     page_icon="📄",
-    layout="wide"
+    layout="wide",
 )
-
-st.title("🤖 AI Resume Analyzer")
-st.write("Welcome! Our project setup is complete.")
